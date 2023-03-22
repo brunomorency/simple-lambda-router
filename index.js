93da09c5956a45345b3cab02b4ffcd97fcf70c4b
Bruno Morency <bruno@fauxplats.com>
2023-03-22 09:49:05 -0400

    Use ES6 module export declaration

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-'use strict'
-
 class RouteError extends Error {
   constructor({statusCode, message}) {
     super(message)
@@ -7,156 +5,153 @@ class RouteError extends Error {
   }
 }
 
-module.exports = {
+export const error = RouteError;
 
-  error: RouteError,
+export const route = (config, ...handlerParams) => (request, context, lambdaCallback) => {
 
-  route: (config, ...handlerParams) => (request, context, lambdaCallback) => {
+  let cfg = Object.assign({}, {
+    resources: null,
+    headers: {},
+    paths: null,
+    debug: false
+  }, config)
+  let _debugLog = (cfg.debug) ? (msg) => console.log(`ROUTER: ${msg}`) : () => {};
 
-    let cfg = Object.assign({}, {
-      resources: null,
-      headers: {},
-      paths: null,
-      debug: false
-    }, config)
-    let _debugLog = (cfg.debug) ? (msg) => console.log(`ROUTER: ${msg}`) : () => {};
+  _debugLog(`request: ${JSON.stringify(request)}`)
 
-    _debugLog(`request: ${JSON.stringify(request)}`)
+  if (request.httpMethod.toUpperCase() == 'OPTIONS') {
 
-    if (request.httpMethod.toUpperCase() == 'OPTIONS') {
+    _debugLog(`Response to OPTIONS request`)
+    return lambdaCallback(null, {
+      statusCode: 204,
+      headers: cfg.headers,
+      body: ''
+    })
 
-      _debugLog(`Response to OPTIONS request`)
-      return lambdaCallback(null, {
-        statusCode: 204,
-        headers: cfg.headers,
-        body: ''
-      })
+  } else {
 
-    } else {
+    let genericInternalErrorResponse = {
+      statusCode:500,
+      headers: cfg.headers,
+      body:JSON.stringify({ message: "Interval Server Error" })
+    }
 
-      let genericInternalErrorResponse = {
-        statusCode:500,
-        headers: cfg.headers,
-        body:JSON.stringify({ message: "Interval Server Error" })
-      }
+    let handler = _getRequestHandlerFile(cfg, request)
+    if (handler) {
 
-      let handler = _getRequestHandlerFile(cfg, request)
-      if (handler) {
+      try {
 
-        try {
-
-          if (cfg.debug) {
-            if (handler.type == 'resource') {
-              _debugLog(`[method: ${request.httpMethod}, resource: ${request.resource}] Executing matching resource handler ${cfg.resources[handler.key]}`)
-            } else {
-              _debugLog(`[method: ${request.httpMethod}, path: ${request.path}] Executing matching path handler ${cfg.paths[handler.key]}`)
-            }
+        if (cfg.debug) {
+          if (handler.type == 'resource') {
+            _debugLog(`[method: ${request.httpMethod}, resource: ${request.resource}] Executing matching resource handler ${cfg.resources[handler.key]}`)
+          } else {
+            _debugLog(`[method: ${request.httpMethod}, path: ${request.path}] Executing matching path handler ${cfg.paths[handler.key]}`)
           }
-
-          if (request.headers) {
-            let requestContentType = request.headers[Object.keys(request.headers).find(header => header.toLowerCase() == 'content-type')]
-            if (requestContentType && typeof requestContentType == 'string' && requestContentType.search(/^application\/json(;|$)/) === 0) {
-              request.body = JSON.parse(request.body)
-            }
-          }
-
-          _runImplementation((handler.type == 'resource') ? cfg.resources[handler.key] : cfg.paths[handler.key])
-          .then(response => {
-
-            _debugLog(`handler success: ${JSON.stringify(response)}`)
-            // apply headers from config and augment with headers from handler response
-            if (!('headers' in response)) {
-              response.headers = cfg.headers
-            }
-            else {
-              response.headers = Object.assign({}, cfg.headers, response.headers)
-            }
-
-            if (response.body && typeof response.body == 'object') {
-              response.body = JSON.stringify(response.body)
-              response.headers['Content-Type'] = 'application/json'
-            } else {
-              response.body = response.body || ''
-            }
-
-            return lambdaCallback(null, response)
-          })
-          .catch(err => {
-
-            _debugLog(`Error: ${err.message}`)
-            if (err instanceof RouteError) {
-              return lambdaCallback(null,{
-                statusCode: err.statusCode,
-                headers: cfg.headers,
-                body:JSON.stringify({ message: err.message })
-              })
-            } else {
-              return lambdaCallback(null, genericInternalErrorResponse)
-            }
-
-          })
-
-          function _runImplementation(handler) {
-            if (Array.isArray(handler)) {
-              return handler.reduce((p, stepFile) => p.then(previousStepOutput => {
-                return _runImplementationStep(require(stepFile), previousStepOutput)
-              }), Promise.resolve(null))
-            } else {
-              return _runImplementationStep(require(handler))
-            }
-          }
-
-          function _runImplementationStep(step, previousStepOutput=null) {
-            // validate request parameters
-            if ('validate' in step) {
-              for (let part in step.validate) {
-                _debugLog(`Validating request.${part}`)
-                let {error, value} = step.validate[part].validate(request[part] || {});
-                if (error) {
-                  // parts of the request isn't valid
-                  _debugLog(`Validation error: ${JSON.stringify(error.details)}`)
-                  return Promise.reject(new RouteError({
-                    statusCode: 400,
-                    message: error.details.map(e => e.message).join('. ')
-                  }))
-                } else {
-                  request[part] = value
-                }
-              }
-            }
-            // handle the API request
-            if (previousStepOutput) {
-              return step.handler(request, context, previousStepOutput, ...handlerParams)
-            } else {
-              return step.handler(request, context, ...handlerParams)
-            }
-          }
-
-        } catch (err) {
-          _debugLog(`handler error:`, err.message)
-          return lambdaCallback(null, genericInternalErrorResponse)
         }
 
-      } else {
-        _debugLog(`No handler defined for method ${request.httpMethod} on resource ${request.resource} or path ${request.path}`)
+        if (request.headers) {
+          let requestContentType = request.headers[Object.keys(request.headers).find(header => header.toLowerCase() == 'content-type')]
+          if (requestContentType && typeof requestContentType == 'string' && requestContentType.search(/^application\/json(;|$)/) === 0) {
+            request.body = JSON.parse(request.body)
+          }
+        }
 
-        // If there is at least one method defined for the requested resource
-        // or path we'll return a 405 response. Otherwise, we'll return a 404
-        let exists = cfg.resources && Object.keys(cfg.resources).some(r => {
-          let [method, resource] = r.split(':')
-          return (resource === request.resource)
+        _runImplementation((handler.type == 'resource') ? cfg.resources[handler.key] : cfg.paths[handler.key])
+        .then(response => {
+
+          _debugLog(`handler success: ${JSON.stringify(response)}`)
+          // apply headers from config and augment with headers from handler response
+          if (!('headers' in response)) {
+            response.headers = cfg.headers
+          }
+          else {
+            response.headers = Object.assign({}, cfg.headers, response.headers)
+          }
+
+          if (response.body && typeof response.body == 'object') {
+            response.body = JSON.stringify(response.body)
+            response.headers['Content-Type'] = 'application/json'
+          } else {
+            response.body = response.body || ''
+          }
+
+          return lambdaCallback(null, response)
         })
-        if (!exists && cfg.paths) exists = Object.keys(cfg.paths).some(p => {
-          return _pathMatchesPattern(request.path, p.split(':').pop())
+        .catch(err => {
+
+          _debugLog(`Error: ${err.message}`)
+          if (err instanceof RouteError) {
+            return lambdaCallback(null,{
+              statusCode: err.statusCode,
+              headers: cfg.headers,
+              body:JSON.stringify({ message: err.message })
+            })
+          } else {
+            return lambdaCallback(null, genericInternalErrorResponse)
+          }
+
         })
 
-        let response = (exists) ? [405, 'Method Not Allowed'] : [404, 'Not Found']
-        return lambdaCallback(null, {
-          statusCode: response[0],
-          headers: cfg.headers,
-          body:JSON.stringify({ message: response[1] })
-        })
+        function _runImplementation(handler) {
+          if (Array.isArray(handler)) {
+            return handler.reduce((p, stepFile) => p.then(previousStepOutput => {
+              return _runImplementationStep(require(stepFile), previousStepOutput)
+            }), Promise.resolve(null))
+          } else {
+            return _runImplementationStep(require(handler))
+          }
+        }
+
+        function _runImplementationStep(step, previousStepOutput=null) {
+          // validate request parameters
+          if ('validate' in step) {
+            for (let part in step.validate) {
+              _debugLog(`Validating request.${part}`)
+              let {error, value} = step.validate[part].validate(request[part] || {});
+              if (error) {
+                // parts of the request isn't valid
+                _debugLog(`Validation error: ${JSON.stringify(error.details)}`)
+                return Promise.reject(new RouteError({
+                  statusCode: 400,
+                  message: error.details.map(e => e.message).join('. ')
+                }))
+              } else {
+                request[part] = value
+              }
+            }
+          }
+          // handle the API request
+          if (previousStepOutput) {
+            return step.handler(request, context, previousStepOutput, ...handlerParams)
+          } else {
+            return step.handler(request, context, ...handlerParams)
+          }
+        }
+
+      } catch (err) {
+        _debugLog(`handler error:`, err.message)
+        return lambdaCallback(null, genericInternalErrorResponse)
       }
+
+    } else {
+      _debugLog(`No handler defined for method ${request.httpMethod} on resource ${request.resource} or path ${request.path}`)
+
+      // If there is at least one method defined for the requested resource
+      // or path we'll return a 405 response. Otherwise, we'll return a 404
+      let exists = cfg.resources && Object.keys(cfg.resources).some(r => {
+        let [method, resource] = r.split(':')
+        return (resource === request.resource)
+      })
+      if (!exists && cfg.paths) exists = Object.keys(cfg.paths).some(p => {
+        return _pathMatchesPattern(request.path, p.split(':').pop())
+      })
+
+      let response = (exists) ? [405, 'Method Not Allowed'] : [404, 'Not Found']
+      return lambdaCallback(null, {
+        statusCode: response[0],
+        headers: cfg.headers,
+        body:JSON.stringify({ message: response[1] })
+      })
     }
   }
 }
